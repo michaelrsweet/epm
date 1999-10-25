039086db99a50115a5dc674ba2dea5ee47ec072e
nobody <nobody@30d6ef08-1af0-0310-a783-d969ace860ab>
1999-10-25 12:46:15 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1.0.1'.

---
+++ tags/release-1.0.1/string.h
@@ -0,0 +1,45 @@
+/*
+ * "$Id: string.h,v 1.1 1999/06/21 14:25:19 mike Exp $"
+ *
+ *   String definitions for the ESP Package Manager (EPM).
+ *
+ *   Copyright 1999 by Easy Software Products.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+/*
+ * Include necessary headers...
+ */
+
+#include "config.h"
+#include <string.h>
+
+
+/*
+ * Prototypes...
+ */
+
+#ifndef HAVE_STRDUP
+extern char	*strdup(const char *);
+#endif /* !HAVE_STRDUP */
+
+#ifndef HAVE_STRCASECMP
+extern int	strcasecmp(const char *, const char *);
+#endif /* !HAVE_STRCASECMP */
+
+#ifndef HAVE_STRNCASECMP
+extern int	strncasecmp(const char *, const char *, size_t n);
+#endif /* !HAVE_STRNCASECMP */
+
+/*
+ * End of "$Id: string.h,v 1.1 1999/06/21 14:25:19 mike Exp $".
+ */
