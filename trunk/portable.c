ec240570f124e0bcc4402355c370ad4e51243bca
mike <mike@30d6ef08-1af0-0310-a783-d969ace860ab>
2002-06-07 21:15:05 +0000

    Fix removal script bug - init script didn't get removed properly.

--- trunk/portable.c
+++ trunk/portable.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: portable.c,v 1.69 2002/06/05 17:51:23 mike Exp $"
+ * "$Id: portable.c,v 1.70 2002/06/07 21:15:05 mike Exp $"
  *
  *   Portable package gateway for the ESP Package Manager (EPM).
  *
@@ -1881,7 +1881,7 @@ write_remove(dist_t     *dist,		/* I - Software distribution */
     fputs("	fi\n", scriptfile);
     fputs("else\n", scriptfile);
     fputs("	for file in", scriptfile);
-    for (; i > 0; i --, file ++)
+    for (i = dist->num_files, file = dist->files; i > 0; i --, file ++)
       if (tolower(file->type) == 'i')
         fprintf(scriptfile, " %s", file->dst);
 
@@ -2083,5 +2083,5 @@ write_space_checks(const char *prodname,/* I - Distribution name */
 
 
 /*
- * End of "$Id: portable.c,v 1.69 2002/06/05 17:51:23 mike Exp $".
+ * End of "$Id: portable.c,v 1.70 2002/06/07 21:15:05 mike Exp $".
  */
