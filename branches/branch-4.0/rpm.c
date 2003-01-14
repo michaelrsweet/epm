8f62b8d1f032f402b84e61256223ec5852f6275f
mike <mike@30d6ef08-1af0-0310-a783-d969ace860ab>
2003-01-14 16:53:04 +0000

    Mirror 3.x changes.

--- branches/branch-4.0/rpm.c
+++ branches/branch-4.0/rpm.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: rpm.c,v 1.35.2.10 2003/01/04 05:10:12 mike Exp $"
+ * "$Id: rpm.c,v 1.35.2.11 2003/01/14 16:53:04 mike Exp $"
  *
  *   Red Hat package gateway for the ESP Package Manager (EPM).
  *
@@ -51,9 +51,8 @@ make_rpm(const char     *prodname,	/* I - Product short name */
   char		current[1024];		/* Current directory */
   const char	*runlevels;		/* Run levels */
   int		number;			/* Start/stop number */
-#ifndef EPM_RPMTOPDIR
-  static char	rpmdir[1024];		/* RPMDIR env var */
-#endif /* EPM_RPMTOPDIR */
+  char		rpmdir[1024];		/* RPMDIR env var */
+  const char	*dname;			/* Dependency name */
 
 
   if (Verbosity)
@@ -139,12 +138,17 @@ make_rpm(const char     *prodname,	/* I - Product short name */
 
   for (i = dist->num_depends, d = dist->depends; i > 0; i --, d ++)
   {
-    if (d->type == DEPEND_REQUIRES)
-      fprintf(fp, "Requires: %s", d->product);
-    else if (d->type == DEPEND_PROVIDES)
-      fprintf(fp, "Provides: %s", d->product);
+    if ((dname = strrchr(d->product, '/')) != NULL)
+      dname ++;
     else
-      fprintf(fp, "Conflicts: %s", d->product);
+      dname = d->product;
+
+    if (d->type == DEPEND_REQUIRES)
+      fprintf(fp, "Requires: %s", dname);
+    else if (d->type == DEPEND_PROVIDES)
+      fprintf(fp, "Provides: %s", dname);
+    else
+      fprintf(fp, "Conflicts: %s", dname);
 
     if (d->vernumber[0] == 0)
     {
@@ -397,11 +401,11 @@ make_rpm(const char     *prodname,	/* I - Product short name */
 
   if (strcmp(platform->machine, "intel") == 0)
   {
-    if (run_command(NULL, "rpm %s -bb " EPM_RPMARCH "i386 %s",
+    if (run_command(NULL, EPM_RPMBUILD " %s -bb " EPM_RPMARCH "i386 %s",
                     Verbosity == 0 ? "--quiet" : "", specname))
       return (1);
   }
-  else if (run_command(NULL, "rpm %s -bb " EPM_RPMARCH "%s %s",
+  else if (run_command(NULL, EPM_RPMBUILD " %s -bb " EPM_RPMARCH "%s %s",
                        Verbosity == 0 ? "--quiet" : "", platform->machine,
 		       specname))
     return (1);
@@ -440,5 +444,5 @@ make_rpm(const char     *prodname,	/* I - Product short name */
 
 
 /*
- * End of "$Id: rpm.c,v 1.35.2.10 2003/01/04 05:10:12 mike Exp $".
+ * End of "$Id: rpm.c,v 1.35.2.11 2003/01/14 16:53:04 mike Exp $".
  */
