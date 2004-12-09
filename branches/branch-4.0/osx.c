d37f8c64f3ea6d24ca858d83b3520955070b879f
mike <mike@30d6ef08-1af0-0310-a783-d969ace860ab>
2004-12-09 17:38:08 +0000

    MacOS X subpackage support.

--- branches/branch-4.0/osx.c
+++ branches/branch-4.0/osx.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: osx.c,v 1.1.2.14 2004/12/07 16:33:18 mike Exp $"
+ * "$Id: osx.c,v 1.1.2.15 2004/12/09 17:38:08 mike Exp $"
  *
  *   MacOS X package gateway for the ESP Package Manager (EPM).
  *
@@ -40,6 +40,14 @@ static const char * const pm_paths[] =	/* Paths to PackageMaker program */
 
 
 /*
+ * Local functions...
+ */
+
+static int	make_subpackage(const char *prodname, const char *directory,
+		                dist_t *dist, const char *subpackage);
+
+
+/*
  * 'make_osx()' - Make a Red Hat software distribution package.
  */
 
@@ -52,8 +60,231 @@ make_osx(const char     *prodname,	/* I - Product short name */
 {
   int		i;			/* Looping var */
   FILE		*fp;			/* Spec file */
-  char		name[1024];		/* Full product name */
   char		filename[1024];		/* Destination filename */
+  char		current[1024];		/* Current directory */
+
+
+  REF(platname);
+  REF(platform);
+
+ /*
+  * Create the main package and subpackages (if any)...
+  */
+
+  if (make_subpackage(prodname, directory, dist, NULL))
+    return (1);
+
+  for (i = 0; i < dist->num_subpackages; i ++)
+    if (make_subpackage(prodname, directory, dist, dist->subpackages[i]))
+      return (1);
+
+  if (dist->num_subpackages)
+  {
+   /*
+    * Create a meta package for the whole shebang...
+    */
+
+    if (Verbosity)
+      puts("Creating MacOS X metapackage...");
+
+    getcwd(current, sizeof(current));
+
+   /*
+    * Copy the resources for the license, readme, and welcome (description)
+    * stuff...
+    */
+
+    if (Verbosity)
+      puts("Copying temporary resource files...");
+
+    snprintf(filename, sizeof(filename), "%s/MetaResources", directory);
+    make_directory(filename, 0777, 0, 0);
+
+    snprintf(filename, sizeof(filename), "%s/MetaResources/License.txt",
+             directory);
+    copy_file(filename, dist->license, 0644, 0, 0);
+
+    snprintf(filename, sizeof(filename), "%s/MetaResources/ReadMe.txt",
+             directory);
+    copy_file(filename, dist->readme, 0644, 0, 0);
+
+    snprintf(filename, sizeof(filename), "%s/MetaResources/Welcome.txt",
+             directory);
+    if ((fp = fopen(filename, "w")) == NULL)
+    {
+      fprintf(stderr, "epm: Unable to create welcome file \"%s\" - %s\n",
+              filename, strerror(errno));
+      return (1);
+    }
+
+    fprintf(fp, "%s version %s\n", dist->product, dist->version);
+    fprintf(fp, "Copyright %s\n", dist->copyright);
+
+    for (i = 0; i < dist->num_descriptions; i ++)
+      if (!dist->descriptions[i].subpackage)
+        fprintf(fp, "%s\n", dist->descriptions[i].description);
+
+    fclose(fp);
+
+    snprintf(filename, sizeof(filename), "%s/%s-metadesc.plist", directory,
+             prodname);
+    if ((fp = fopen(filename, "w")) == NULL)
+    {
+      fprintf(stderr, "epm: Unable to create description file \"%s\" - %s\n",
+              filename, strerror(errno));
+      return (1);
+    }
+
+    fputs("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", fp);
+    fputs("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n", fp);
+    fputs("<plist version=\"1.0\">\n", fp);
+    fputs("<dict>\n", fp);
+    fputs("        <key>IFPkgDescriptionDeleteWarning</key>\n", fp);
+    fputs("        <string></string>\n", fp);
+    fputs("        <key>IFPkgDescriptionDescription</key>\n", fp);
+    fputs("        <string>", fp);
+    for (i = 0; i < dist->num_descriptions; i ++)
+      if (!dist->descriptions[i].subpackage)
+        fprintf(fp, "%s\n", dist->descriptions[i].description);
+    fputs("</string>\n", fp);
+    fputs("        <key>IFPkgDescriptionTitle</key>\n", fp);
+    fprintf(fp, "        <string>%s</string>\n", dist->product);
+    fputs("        <key>IFPkgDescriptionVersion</key>\n", fp);
+    fprintf(fp, "        <string>%s</string>\n", dist->version);
+    fputs("</dict>\n", fp);
+    fputs("</plist>\n", fp);
+
+    fclose(fp);
+
+   /*
+    * Do the info file for the packager...
+    */
+
+    snprintf(filename, sizeof(filename), "%s/%s-metainfo.plist", directory,
+             prodname);
+    if ((fp = fopen(filename, "w")) == NULL)
+    {
+      fprintf(stderr, "epm: Unable to create package information file \"%s\" - %s\n",
+              filename, strerror(errno));
+      return (1);
+    }
+
+    fputs("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", fp);
+    fputs("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n", fp);
+    fputs("<plist version=\"1.0\">\n", fp);
+    fputs("<dict>\n", fp);
+    fputs("        <key>IFPkgFormatVersion</key>\n", fp);
+    fputs("        <real>0.1</real>\n", fp);
+    fputs("        <key>IFPkgFlagAuthorizationAction</key>\n", fp);
+    fputs("        <string>RootAuthorization</string>\n", fp);
+    fputs("        <key>IFPkgFlagComponentDirectory</key>\n", fp);
+    fputs("        <string>../Packages</string>\n", fp);
+    fputs("        <key>IFPkgFlagPackageList</key>\n", fp);
+    fputs("        <array>\n", fp);
+    fputs("                <dict>\n", fp);
+    fputs("                        <key>IFPkgFlagPackageLocation</key>\n", fp);
+    fprintf(fp, "                        <string>%s.pkg</string>\n", prodname);
+    fputs("                        <key>IFPkgFlagPackageSelection</key>\n", fp);
+    fputs("                        <string>selected</string>\n", fp);
+    fputs("                </dict>\n", fp);
+    for (i = 0; i < dist->num_subpackages; i ++)
+    {
+      fputs("                <dict>\n", fp);
+      fputs("                        <key>IFPkgFlagPackageLocation</key>\n", fp);
+      fprintf(fp, "                        <string>%s-%s.pkg</string>\n",
+              prodname, dist->subpackages[i]);
+      fputs("                </dict>\n", fp);
+    }
+    fputs("        </array>\n", fp);
+    fputs("        <key>CFBundleName</key>\n", fp);
+    fprintf(fp, "        <string>%s</string>\n", dist->product);
+    fputs("        <key>CFBundleGetInfoString</key>\n", fp);
+    fprintf(fp, "        <string>%s %s</string>\n", dist->product, dist->version);
+    fputs("        <key>CFBundleShortVersionString</key>\n", fp);
+    fprintf(fp, "        <string>%s</string>\n", dist->version);
+    fputs("</dict>\n", fp);
+    fputs("</plist>\n", fp);
+
+    fclose(fp);
+
+   /*
+    * Build the distribution...
+    */
+
+    if (Verbosity)
+      puts("Building OSX package...");
+
+    if (directory[0] == '/')
+      strlcpy(filename, directory, sizeof(filename));
+    else
+      snprintf(filename, sizeof(filename), "%s/%s", current, directory);
+
+    for (i = 0; pm_paths[i]; i ++)
+      if (!access(pm_paths[i], 0))
+	break;
+
+    if (pm_paths[i])
+      run_command(NULL, "%s/Contents/MacOS/PackageMaker -build "
+			"-p %s/%s.mpkg "
+			"-r %s/MetaResources "
+			"-d %s/%s-metadesc.plist "
+			"-i %s/%s-metainfo.plist",
+		  pm_paths[i],
+		  filename, prodname,
+		  filename,
+		  filename, prodname,
+		  filename, prodname);
+    else
+    {
+      fputs("epm: Unable to find \"PackageMaker\" program!\n", stderr);
+      return (1);
+    }
+
+    snprintf(filename, sizeof(filename), "%s/%s.mpkg", directory, prodname);
+    if (access(filename, 0))
+      return (1);
+
+   /*
+    * Remove temporary files...
+    */
+
+    if (!KeepFiles)
+    {
+      if (Verbosity)
+	puts("Removing temporary distribution files...");
+
+      run_command(NULL, "/bin/rm -rf %s/MetaResources", directory);
+
+      snprintf(filename, sizeof(filename), "%s/%s-metadesc.plist", directory,
+               prodname);
+      unlink(filename);
+
+      snprintf(filename, sizeof(filename), "%s/%s-metainfo.plist", directory,
+               prodname);
+      unlink(filename);
+    }
+  }
+
+  return (0);
+}
+
+
+/*
+ * 'make_subpackage()' - Make a OSX subpackage.
+ */
+
+static int
+make_subpackage(const char *prodname,	/* I - Product short name */
+                const char *directory,	/* I - Directory for distribution files */
+		dist_t     *dist,	/* I - Distribution  information */
+                const char *subpackage)	/* I - Subpackage */
+{
+  int		i;			/* Looping var */
+  FILE		*fp;			/* Spec file */
+  char		prodfull[1024],		/* Full product name */
+		filename[1024],		/* Destination filename */
+		pkgdir[1024],		/* Package directory */
+		pkgname[1024];		/* Package name */
   file_t	*file;			/* Current distribution file */
   command_t	*c;			/* Current command */
   struct passwd	*pwd;			/* Pointer to user record */
@@ -62,24 +293,15 @@ make_osx(const char     *prodname,	/* I - Product short name */
   const char	*option;		/* Init script option */
 
 
+  if (subpackage)
+    snprintf(prodfull, sizeof(prodfull), "%s-%s", prodname, subpackage);
+  else
+    strlcpy(prodfull, prodname, sizeof(prodfull));
+
   if (Verbosity)
-    puts("Creating MacOS X distribution...");
+    printf("Creating %s MacOS X package...\n", prodfull);
 
   getcwd(current, sizeof(current));
-
-  if (dist->relnumber)
-  {
-    if (platname[0])
-      snprintf(name, sizeof(name), "%s-%s-%d-%s", prodname, dist->version,
-               dist->relnumber, platname);
-    else
-      snprintf(name, sizeof(name), "%s-%s-%d", prodname, dist->version,
-               dist->relnumber);
-  }
-  else if (platname[0])
-    snprintf(name, sizeof(name), "%s-%s-%s", prodname, dist->version, platname);
-  else
-    snprintf(name, sizeof(name), "%s-%s", prodname, dist->version);
 
  /*
   * Copy the resources for the license, readme, and welcome (description)
@@ -89,16 +311,19 @@ make_osx(const char     *prodname,	/* I - Product short name */
   if (Verbosity)
     puts("Copying temporary resource files...");
 
-  snprintf(filename, sizeof(filename), "%s/Resources", directory);
+  snprintf(filename, sizeof(filename), "%s/%s/Resources", directory, prodfull);
   make_directory(filename, 0777, 0, 0);
 
-  snprintf(filename, sizeof(filename), "%s/Resources/License.txt", directory);
+  snprintf(filename, sizeof(filename), "%s/%s/Resources/License.txt",
+           directory, prodfull);
   copy_file(filename, dist->license, 0644, 0, 0);
 
-  snprintf(filename, sizeof(filename), "%s/Resources/ReadMe.txt", directory);
+  snprintf(filename, sizeof(filename), "%s/%s/Resources/ReadMe.txt",
+           directory, prodfull);
   copy_file(filename, dist->readme, 0644, 0, 0);
 
-  snprintf(filename, sizeof(filename), "%s/Resources/Welcome.txt", directory);
+  snprintf(filename, sizeof(filename), "%s/%s/Resources/Welcome.txt",
+           directory, prodfull);
   if ((fp = fopen(filename, "w")) == NULL)
   {
     fprintf(stderr, "epm: Unable to create welcome file \"%s\" - %s\n",
@@ -110,11 +335,12 @@ make_osx(const char     *prodname,	/* I - Product short name */
   fprintf(fp, "Copyright %s\n", dist->copyright);
 
   for (i = 0; i < dist->num_descriptions; i ++)
-    fprintf(fp, "%s\n", dist->descriptions[i].description);
+    if (dist->descriptions[i].subpackage == subpackage)
+      fprintf(fp, "%s\n", dist->descriptions[i].description);
 
   fclose(fp);
 
-  snprintf(filename, sizeof(filename), "%s/%s-desc.plist", directory, prodname);
+  snprintf(filename, sizeof(filename), "%s/%s-desc.plist", directory, prodfull);
   if ((fp = fopen(filename, "w")) == NULL)
   {
     fprintf(stderr, "epm: Unable to create description file \"%s\" - %s\n",
@@ -131,7 +357,8 @@ make_osx(const char     *prodname,	/* I - Product short name */
   fputs("        <key>IFPkgDescriptionDescription</key>\n", fp);
   fputs("        <string>", fp);
   for (i = 0; i < dist->num_descriptions; i ++)
-    fprintf(fp, "%s\n", dist->descriptions[i].description);
+    if (dist->descriptions[i].subpackage == subpackage)
+      fprintf(fp, "%s\n", dist->descriptions[i].description);
   fputs("</string>\n", fp);
   fputs("        <key>IFPkgDescriptionTitle</key>\n", fp);
   fprintf(fp, "        <string>%s</string>\n", dist->product);
@@ -146,7 +373,7 @@ make_osx(const char     *prodname,	/* I - Product short name */
   * Do the info file for the packager...
   */
 
-  snprintf(filename, sizeof(filename), "%s/%s-info.plist", directory, prodname);
+  snprintf(filename, sizeof(filename), "%s/%s-info.plist", directory, prodfull);
   if ((fp = fopen(filename, "w")) == NULL)
   {
     fprintf(stderr, "epm: Unable to create package information file \"%s\" - %s\n",
@@ -162,6 +389,11 @@ make_osx(const char     *prodname,	/* I - Product short name */
   fputs("        <real>0.1</real>\n", fp);
   fputs("        <key>IFPkgFlagAuthorizationAction</key>\n", fp);
   fputs("        <string>RootAuthorization</string>\n", fp);
+  if (dist->num_subpackages)
+  {
+    fputs("        <key>IFPkgFlagIsRequired</key>\n", fp);
+    fputs("        <true/>\n", fp);
+  }
   fputs("        <key>CFBundleName</key>\n", fp);
   fprintf(fp, "        <string>%s</string>\n", dist->product);
   fputs("        <key>CFBundleGetInfoString</key>\n", fp);
@@ -178,12 +410,13 @@ make_osx(const char     *prodname,	/* I - Product short name */
   */
 
   for (i = dist->num_commands, c = dist->commands; i > 0; i --, c ++)
-    if (c->type == COMMAND_PRE_INSTALL)
+    if (c->type == COMMAND_PRE_INSTALL && c->subpackage == subpackage)
       break;
 
   if (i)
   {
-    snprintf(filename, sizeof(filename), "%s/Resources/preflight", directory);
+    snprintf(filename, sizeof(filename), "%s/%s/Resources/preflight",
+             directory, prodfull);
     if ((fp = fopen(filename, "w")) == NULL)
     {
       fprintf(stderr, "epm: Unable to create preinstall script \"%s\" - %s\n",
@@ -194,7 +427,7 @@ make_osx(const char     *prodname,	/* I - Product short name */
     fputs("#!/bin/sh\n", fp);
 
     for (; i > 0; i --, c ++)
-      if (c->type == COMMAND_PRE_INSTALL)
+      if (c->type == COMMAND_PRE_INSTALL && c->subpackage == subpackage)
 	fprintf(fp, "%s\n", c->command);
 
     fclose(fp);
@@ -202,19 +435,20 @@ make_osx(const char     *prodname,	/* I - Product short name */
   }
 
   for (i = dist->num_commands, c = dist->commands; i > 0; i --, c ++)
-    if (c->type == COMMAND_POST_INSTALL)
+    if (c->type == COMMAND_POST_INSTALL && c->subpackage == subpackage)
       break;
 
   if (!i)
   {
     for (i = dist->num_files, file = dist->files; i > 0; i --, file ++)
-      if (tolower(file->type) == 'i')
+      if (tolower(file->type) == 'i' && file->subpackage == subpackage)
         break;
   }
 
   if (i)
   {
-    snprintf(filename, sizeof(filename), "%s/Resources/postflight", directory);
+    snprintf(filename, sizeof(filename), "%s/%s/Resources/postflight",
+             directory, prodfull);
     if ((fp = fopen(filename, "w")) == NULL)
     {
       fprintf(stderr, "epm: Unable to create postinstall script \"%s\" - %s\n",
@@ -225,11 +459,11 @@ make_osx(const char     *prodname,	/* I - Product short name */
     fputs("#!/bin/sh\n", fp);
 
     for (i = dist->num_commands, c = dist->commands; i > 0; i --, c ++)
-      if (c->type == COMMAND_POST_INSTALL)
+      if (c->type == COMMAND_POST_INSTALL && c->subpackage == subpackage)
 	fprintf(fp, "%s\n", c->command);
 
     for (i = dist->num_files, file = dist->files; i > 0; i --, file ++)
-      if (tolower(file->type) == 'i')
+      if (tolower(file->type) == 'i' && file->subpackage == subpackage)
         qprintf(fp, "/Library/StartupItems/%s/%s start\n",
 	        file->dst, file->dst);
 
@@ -246,6 +480,13 @@ make_osx(const char     *prodname,	/* I - Product short name */
 
   for (i = dist->num_files, file = dist->files; i > 0; i --, file ++)
   {
+   /*
+    * Only add files in this subpackage...
+    */
+
+    if (file->subpackage != subpackage)
+      continue;
+
    /*
     * Find the username and groupname IDs...
     */
@@ -383,24 +624,56 @@ make_osx(const char     *prodname,	/* I - Product short name */
   else
     snprintf(filename, sizeof(filename), "%s/%s", current, directory);
 
-  for (i = 0; paths[i]; i ++)
-    if (!access(paths[i], 0))
+  if (dist->num_subpackages)
+  {
+   /*
+    * Place the individual packages in the "Packages" subdirectory;
+    * a metapackage will handle the whole shebang...
+    */
+
+    snprintf(pkgdir, sizeof(pkgdir), "%s/Packages", filename);
+    make_directory(pkgdir, 0777, 0, 0);
+
+    snprintf(pkgname, sizeof(pkgname), "%s/Packages/%s.pkg", filename,
+             prodfull);
+  }
+  else
+  {
+   /*
+    * The package stands alone - just put it in the output directory...
+    */
+
+    snprintf(pkgname, sizeof(pkgname), "%s/%s.pkg", filename, prodfull);
+  }
+
+  for (i = 0; pm_paths[i]; i ++)
+    if (!access(pm_paths[i], 0))
       break;
 
-  if (paths[i])
+  if (pm_paths[i])
     run_command(NULL, "%s/Contents/MacOS/PackageMaker -build "
-		      "-p %s/%s.pkg -f %s/Package -r %s/Resources "
-		      "-d %s/%s-desc.plist -i %s/%s-info.plist",
-		paths[i], filename, prodname, filename, filename, filename,
-		prodname, filename, prodname);
+		      "-p %s "
+		      "-f %s/%s/Package "
+		      "-r %s/%s/Resources "
+		      "-d %s/%s-desc.plist "
+		      "-i %s/%s-info.plist",
+		pm_paths[i],
+		pkgname,
+		filename, prodfull,
+		filename, prodfull,
+		filename, prodfull,
+		filename, prodfull);
   else
   {
     fputs("epm: Unable to find \"PackageMaker\" program!\n", stderr);
     return (1);
   }
 
-  snprintf(filename, sizeof(filename), "%s/%s.pkg", directory, prodname);
-  if (access(filename, 0))
+ /*
+  * Verify that the package was created...
+  */
+
+  if (access(pkgname, 0))
     return (1);
 
  /*
@@ -412,13 +685,14 @@ make_osx(const char     *prodname,	/* I - Product short name */
     if (Verbosity)
       puts("Removing temporary distribution files...");
 
-    run_command(NULL, "/bin/rm -rf %s/Resources", directory);
-    run_command(NULL, "/bin/rm -rf %s/Package", directory);
+    run_command(NULL, "/bin/rm -rf %s/%s", directory, prodfull);
 
-    snprintf(filename, sizeof(filename), "%s/%s-desc.plist", directory, prodname);
+    snprintf(filename, sizeof(filename), "%s/%s-desc.plist", directory,
+             prodfull);
     unlink(filename);
 
-    snprintf(filename, sizeof(filename), "%s/%s-info.plist", directory, prodname);
+    snprintf(filename, sizeof(filename), "%s/%s-info.plist", directory,
+             prodfull);
     unlink(filename);
   }
 
@@ -427,5 +701,5 @@ make_osx(const char     *prodname,	/* I - Product short name */
 
 
 /*
- * End of "$Id: osx.c,v 1.1.2.14 2004/12/07 16:33:18 mike Exp $".
+ * End of "$Id: osx.c,v 1.1.2.15 2004/12/09 17:38:08 mike Exp $".
  */
