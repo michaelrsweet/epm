ccd5bd6cc1ab069a25306f66f237d8c9cf50237c
msweet <msweet@30d6ef08-1af0-0310-a783-d969ace860ab>
2014-02-03 00:35:08 +0000

    Add native Cocoa Uninstall application for EPM.

---
+++ branches/branch-4/xcode/Uninstall/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Uninstall
+//
+//  Created by Michael Sweet on 2/1/2014.
+//  Copyright (c) 2014 Michael Sweet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
